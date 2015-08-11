68910816cd40d57fe9823fa326c44ee02f437691
hitechverma <hitechlov22@gmail.com>
2015-08-12 02:30:19 +0530

    newf

--- funnystring.py
+++ funnystring.py
@@ -4,7 +4,11 @@ def funny(stri):
 	r = len(mylist)/2
 	for y in range(0,r):
 		if (ord(mylist[r-y])-ord(mylist[y]) == ord(mylist[r-y-1])-ord(mylist[y-1])):
-			print "yes"
+			print "funny"
+			break
+		else:
+			print "not funny"
+			break
 
 for x in range(0,noc):
 	stri = raw_input("string")
