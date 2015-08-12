a92a35bb72d1b8f43ac281107b0367da17b07411
hitechverma <hitechlov22@gmail.com>
2015-08-13 01:47:52 +0530

    acxz

--- funnystring.py
+++ funnystring.py
@@ -1,15 +1,19 @@
-noc = int(input("test case"))
+noc = int(input())
 def funny(stri):
 	mylist = (stri)
 	r = len(mylist)/2
-	for y in range(0,r):
-		if (ord(mylist[r-y])-ord(mylist[y]) == ord(mylist[r-y-1])-ord(mylist[y-1])):
+	for y in range(0,r-1):
+		if (ord(mylist[r-y])-ord(mylist[y]) == ord(mylist[r-y-1])-ord(mylist[y+1])):
 			print "funny"
 			break
-		else:
-			print "not funny"
-			break
-
+		# else:
+		# 	print "not funny"
+		# 	break
+list = []
 for x in range(0,noc):
-	stri = raw_input("string")
-	funny(stri)
+	stri = raw_input()
+	rev = stri[::-1]
+	print rev
+	list.append(stri)
+for a in range(0,noc):
+	funny(list[a])
