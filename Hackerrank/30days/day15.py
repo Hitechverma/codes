627f5a76f6a1e501ad89c77247ed2835aec70dc2
hitechverma <hitechlov22@gmail.com>
2016-02-01 04:07:26 +0530

    final linked list

--- Hackerrank/30days/day15.py
+++ Hackerrank/30days/day15.py
@@ -8,3 +8,30 @@ class Solution:
         while current:
             print current.data,
             current = current.next
+
+
+    def insert(self,head,data): 
+    #Complete this method
+		# newNode = Node(data)
+		# newNode.head = self
+		# print newNode.data
+		if head== None:
+			head = Node(data)
+		else:
+			current = head
+			while current.next!=None:
+				current = current.next
+			current.next = Node(data)
+			# current.next = None
+		return head
+
+
+
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head);
